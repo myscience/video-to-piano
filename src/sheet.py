633ce4c3e6c73589159f0bc56ef9c7f8c6cf5041
myscience <muratore.paolo95@gmail.com>
2023-10-26 10:19:24 +0200

    Improvement on sheet music rendering

--- src/sheet.py
+++ src/sheet.py
@@ -16,6 +16,7 @@ def get_score(
     key_mode : str = 'major',
     clef : str | Dict[str, str] = 'treble',
     score_name : str = 'Example Score',
+    manual_bar_line : bool = True,
 ) -> abjad.Score:
     
     bar_line = abjad.BarLine()
@@ -31,41 +32,60 @@ def get_score(
 
         voices = []
         for idx, bar in bars.items():
+            bar_voice = abjad.Voice([], name=f'{name}-bar:{idx}')
             for notes in bar:
                 if len(notes) > 2:
-                    voice = [parse_voice(name, duration) for name, duration in notes.items() if 'T_' not in name]       
-                    voice = abjad.Voice(voice, simultaneous=True)
+                    abj_notes = [parse_note(name, duration) for name, duration in notes.items() if 'T_' not in name]
+
+                    # Convert note to voices to allow for simultaneous playing
+                    # Check whether all durations are equal, in this case we can
+                    # use a chord instead of a voice
+                    if len(abj_notes) == 1: voice = abj_notes[0]
+                    elif all(note.written_duration == abj_notes[0].written_duration for note in abj_notes):
+                        voice = abjad.Chord(*abj_notes)
+                    else:
+                        # Use a voice to allow for simultaneous playing
+                        voice = abjad.Voice([abjad.Voice([note]) for note in abj_notes], simultaneous=True)
 
                 else:
                     # There is no note to play, must be a rest
                     voice = parse_rest(notes['T_FRAME'])
 
-                voices.append(voice)
+                bar_voice.append(voice)
                 
             if idx == 0:
-                abjad.attach(key_signature, voice[0][0])
-                abjad.attach(clef, voice[0][0])
+                abjad.attach(key_signature, get_first_note(bar_voice))
+                abjad.attach(clef, get_first_note(bar_voice))
 
+            abjad.attach(bar_line, bar_voice[-1])
 
+            voices.append(bar_voice)
             # staff.append()
 
         print(voices)
+
         staff = abjad.Staff(voices, name=name)
+
+
+        if manual_bar_line:
+            # Suppress automatic bar lines
+            abjad.override(staff).bar_line.stencil = False
+        
         # abjad.attach(bar_line, staff)
         staffs.append(staff)
 
-    print(staffs)
-    
-    score = abjad.Score(staffs, name=score_name, simultaneous=True)
+    staffs = abjad.StaffGroup(staffs[::-1], lilypond_type='PianoStaff', simultaneous=True)
+        
+    score = abjad.Score([staffs], name=score_name)
 
     return score
 
-def parse_voice(
+def parse_note(
     note : str,
     duration : int,
     central_octave : int = 3,
     min_unit : int = 16,
-) -> abjad.Voice:
+) -> abjad.Note:
     name, octave = note.split('-')
                     
     name = name.lower().replace('#', 's').replace('b', 'f')
@@ -74,9 +94,24 @@ def parse_voice(
     duration = abjad.Duration(duration, min_unit)
     note = abjad.Note(name + pitch, duration)
 
-    return abjad.Voice([note])
+    return note
 
 def parse_rest(
     duration : int,
+    min_unit : int = 16,
 ) -> abjad.Rest():
+    duration = abjad.Duration(duration, min_unit)
+
     return abjad.Rest(duration)
+
+def get_first_note(
+    bar : abjad.Container
+) -> abjad.Note:
+    return abjad.get.leaf(bar)
+
+def get_last_note(
+    bar : abjad.Container
+) -> abjad.Note:
+    note = abjad.get.leaf(bar).notes[-1]
+
+    return note
